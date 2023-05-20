57fcd4511652f86cc8937a6117c819782e4d93a3
csbrendan <ubuntu@ip-172-31-46-111.us-west-2.compute.internal>
2023-05-20 14:45:07 +0000

    cs230 proj in prog

---
+++ chestmnist_benchmark.py
@@ -0,0 +1,291 @@
+from tqdm import tqdm
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision.transforms as transforms
+from torchvision import datasets, models, transforms
+
+import random
+import medmnist
+from medmnist import INFO, Evaluator
+
+
+
+
+print(f"MedMNIST v{medmnist.__version__} @ {medmnist.HOMEPAGE}")
+
+
+
+#choose dataset and wether to download again
+#data_flag = 'pathmnist'
+data_flag = 'chestmnist'
+#download = True
+download = False
+
+
+NUM_EPOCHS = 15
+BATCH_SIZE = 128
+lr = 0.001
+
+info = INFO[data_flag]
+task = info['task']
+n_channels = info['n_channels']
+n_classes = len(info['label'])
+
+DataClass = getattr(medmnist, info['python_class'])
+
+
+
+#Preprocessing
+'''
+data_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[.5], std=[.5])
+])
+'''
+#because ResNet expects RGB not grayscale, we need to convert the images to (normalized) RGB
+data_transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=3),  # Convert to RGB
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # Normalize for RGB
+])
+
+
+
+
+
+# load the data
+train_dataset = DataClass(split='train', transform=data_transform, download=download)
+test_dataset = DataClass(split='test', transform=data_transform, download=download)
+
+pil_dataset = DataClass(split='train', download=download)
+
+# encapsulate data into dataloader form
+train_loader = data.DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+train_loader_at_eval = data.DataLoader(dataset=train_dataset, batch_size=2*BATCH_SIZE, shuffle=False)
+test_loader = data.DataLoader(dataset=test_dataset, batch_size=2*BATCH_SIZE, shuffle=False)
+
+print(train_dataset)
+print("===================")
+print(test_dataset)
+
+
+# visualization
+#train_dataset.montage(length=1)
+
+# montage
+#train_dataset.montage(length=20)
+
+
+
+######## TRANSFER
+
+# Top level data directory. Here we assume the format of the directory conforms
+#   to the ImageFolder structure
+#data_dir = 
+
+# Models to choose from [resnet, alexnet, vgg, squeezenet, densenet, inception]
+model_name = "resnet"
+
+# Number of classes in the dataset
+
+#Description: The ChestMNIST is based on the NIH-ChestXray14 dataset 
+# a dataset comprising 112,120 frontal-view X-Ray images of 30,805 unique patients with the text-mined 14 disease labels, 
+# which could be formulized as a multi-label binary-class classification task. We use the official data split, 
+# and resize the source images of 1×1024×1024 into 1×28×28.
+
+
+num_classes = 2
+
+# Batch size for training (change depending on how much memory you have)
+#batch_size = 8
+
+# Flag for feature extracting. When False, we finetune the whole model,
+#   when True we only update the reshaped layer params
+feature_extract = False
+
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+
+# Imagenet transfer learn to simple CNN model
+#model.fc = nn.Linear(512, num_classes)
+def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True):
+    # Initialize these variables which will be set in this if statement. Each of these
+    #   variables is model specific.
+    model_ft = None
+    input_size = 0
+
+    if model_name == "resnet":
+        """ Resnet18
+        """
+        model_ft = models.resnet18(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    else:
+        print("Invalid model name, exiting...")
+        exit()
+
+    return model_ft, input_size
+
+# Initialize the model for this run
+model_ft, input_size = initialize_model(model_name, num_classes, feature_extract, use_pretrained=True)
+
+# Print the model we just instantiated
+print(model_ft)
+
+######## TRANSFER
+
+
+
+
+
+
+
+
+# reshape the final layer to have same # outputs as the number of classes in new dataset
+
+class Net(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super(Net, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            #nn.Conv2d(resnet_output_size, 16, kernel_size=3),
+            nn.Conv2d(in_channels, 16, kernel_size=3),
+            nn.BatchNorm2d(16),
+            nn.ReLU())
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 16, kernel_size=3),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(16, 64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU())
+        
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU())
+
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.fc = nn.Sequential(
+            nn.Linear(512, 128), 
+            nn.ReLU(),
+            #nn.Linear(512, 128),   
+            #nn.ReLU(),
+            nn.Linear(128, num_classes))   
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+
+# instantiate the model above
+resnet_output_size = model_ft.fc.in_features
+net_model = Net(in_channels=n_channels, num_classes=n_classes)
+    
+# Replace the last layers of Resnet model_ft with the layers from net_model
+model_ft.fc = net_model.fc
+
+# define loss function and optimizer
+if task == "multi-label, binary-class":
+    criterion = nn.BCEWithLogitsLoss()
+else:
+    criterion = nn.CrossEntropyLoss()
+    
+optimizer = optim.SGD(model_ft.parameters(), lr=lr, momentum=0.9)
+
+
+# train
+
+for epoch in range(NUM_EPOCHS):
+    train_correct = 0
+    train_total = 0
+    test_correct = 0
+    test_total = 0
+    
+    #model.train()
+    model_ft.train()
+    for inputs, targets in tqdm(train_loader):
+        # forward + backward + optimize
+        optimizer.zero_grad()
+        outputs = model_ft(inputs) #was model()
+        
+        if task == 'multi-label, binary-class':
+            targets = targets.to(torch.float32)
+            loss = criterion(outputs, targets)
+        else:
+            targets = targets.squeeze().long()
+            loss = criterion(outputs, targets)
+        
+        loss.backward()
+        optimizer.step()
+
+
+#save model to disk
+#PATH = 'test_models/breastmnist_test.pth'
+torch.save(model_ft.state_dict(), 'benchmark_resnet18_chestmnist_15epoch.pth')   #was model.
+
+#not reco, but you can save entire model
+#torch.save(model, 'test_models/breastmnist_test.pth')
+
+#load saved model
+#restored_model = torch.load(PATH)
+
+
+def test(split):
+    model_ft.eval()  #was model.
+    y_true = torch.tensor([])
+    y_score = torch.tensor([])
+    
+    data_loader = train_loader_at_eval if split == 'train' else test_loader
+
+    with torch.no_grad():
+        for inputs, targets in data_loader:
+            outputs = model_ft(inputs)
+
+            if task == 'multi-label, binary-class':
+                targets = targets.to(torch.float32)
+                outputs = outputs.softmax(dim=-1)
+            else:
+                targets = targets.squeeze().long()
+                outputs = outputs.softmax(dim=-1)
+                targets = targets.float().resize_(len(targets), 1)
+
+            y_true = torch.cat((y_true, targets), 0)
+            y_score = torch.cat((y_score, outputs), 0)
+
+        y_true = y_true.numpy()
+        y_score = y_score.detach().numpy()
+        
+        evaluator = Evaluator(data_flag, split)
+        metrics = evaluator.evaluate(y_score)
+    
+        print('%s  auc: %.3f  acc:%.3f' % (split, *metrics))
+
+        
+print('==> Evaluating ...')
+test('train')
+test('test')        
---
+++ train.py
@@ -0,0 +1,167 @@
+import os
+import argparse
+import multiprocessing
+from pathlib import Path
+from PIL import Image
+import numpy as np
+import torch
+from torchvision import models, transforms
+from torch.utils.data import DataLoader, Dataset
+
+from byol_pytorch import BYOL
+import pytorch_lightning as pl 
+
+# resnet18
+resnet = models.resnet18(pretrained=True)
+
+# arguments
+
+parser = argparse.ArgumentParser(description='byol-lightning-test')
+
+parser.add_argument('--image_folder', type=str, required = True,
+                       help='path to your folder of images for self-supervised learning')
+
+args = parser.parse_args()
+
+# constants
+
+BATCH_SIZE = 32
+EPOCHS     = 3 #was 1000
+LR         = 3e-4
+#NUM_GPUS   = 2
+IMAGE_SIZE = 28 #was 256
+IMAGE_EXTS = ['.jpg', '.png', '.jpeg']
+NUM_WORKERS = multiprocessing.cpu_count()
+
+# pytorch lightning module
+
+class SelfSupervisedLearner(pl.LightningModule):
+    def __init__(self, net, **kwargs):
+        super().__init__()
+        self.learner = BYOL(net, **kwargs)
+
+    def forward(self, images):
+        return self.learner(images)
+
+    def training_step(self, images, _):
+        loss = self.forward(images)
+        return {'loss': loss}
+
+    '''#me
+    def validation_step(self, batch, batch_idx):
+        images, labels = batch
+        outputs = self.forward(images)
+        loss = self.calculate_loss(outputs, labels)
+        metrics = self.calculate_metrics(outputs, labels)
+        self.log('val_loss', loss, prog_bar=True)
+        self.log_dict(metrics, prog_bar=True)    
+        #return {'val_loss': loss, 'val_acc': accuracy}  
+    '''
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=LR)
+
+    def on_before_zero_grad(self, _):
+        if self.learner.use_momentum:
+            self.learner.update_moving_average()
+
+    '''#me
+    def calculate_loss(self, outputs, labels):
+        # Implement your loss calculation logic here
+        loss = torch.nn.functional.cross_entropy(outputs, labels)
+        return loss
+    
+    #me
+    def calculate_metrics(self, outputs, labels):
+        # Implement your metrics calculation logic here
+        preds = torch.argmax(outputs, dim=1)
+        accuracy = (preds == labels).float().mean()
+        metrics = {'val_acc': accuracy}
+        return metrics
+    '''
+
+
+# images dataset
+
+def expand_greyscale(t):
+    return t.expand(3, -1, -1)
+
+
+class ImagesDataset(Dataset):
+    def __init__(self, npz_file, image_size, set_flag):
+        super().__init__()
+        '''
+        data = np.load('dermamnist.npz', allow_pickle=True)
+        print(data.files)
+        ->  ['train_images', 'val_images', 'test_images', 'train_labels', 'val_labels', 'test_labels']
+        '''
+        self.npz_file = npz_file
+        self.image_size = image_size
+        self.data = np.load(npz_file)
+
+        if set_flag == 'train':
+            self.images = self.data['train_images']    
+            self.labels = self.data['train_labels']
+        elif set_flag == 'validate':
+            self.images = self.data['val_images']    
+            self.labels = self.data['val_labels']
+        elif set_flag == 'test':
+            self.images = self.data['test_images']    
+            self.labels = self.data['test_labels']
+
+        self.transform = transforms.Compose([
+            transforms.Resize(image_size),
+            transforms.CenterCrop(image_size),
+            transforms.ToTensor(),
+            transforms.Lambda(expand_greyscale)
+        ])
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        image = self.images[index]
+        #label = self.labels[index]
+        img = Image.fromarray(image)
+        img = img.convert('RGB')
+        return self.transform(img)
+
+
+# main
+if __name__ == '__main__':
+
+    npz_file_path = '/home/ubuntu/.medmnist/breastmnist.npz' 
+
+    ds_train = ImagesDataset(npz_file_path, IMAGE_SIZE, 'train')
+    ds_validate = ImagesDataset(npz_file_path, IMAGE_SIZE, 'validate')
+    ds_test = ImagesDataset(npz_file_path, IMAGE_SIZE, 'test')
+
+    #ds = ImagesDataset(args.image_folder, IMAGE_SIZE)
+
+    train_loader = DataLoader(ds_train, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=True)
+
+    val_loader = DataLoader(ds_validate, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=False)
+
+    test_loader = DataLoader(ds_test, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=False)
+
+
+    model = SelfSupervisedLearner(
+        resnet,
+        image_size = IMAGE_SIZE,
+        hidden_layer = 'avgpool',
+        projection_size = 256,       # it is good to experiment with this size, tho it should be smaller than my input dimension of 28x28x3=2352
+        projection_hidden_size = 4096,
+        moving_average_decay = 0.99
+    )
+
+    trainer = pl.Trainer(
+        #gpus = NUM_GPUS,
+        max_epochs = EPOCHS,
+        accumulate_grad_batches = 1,
+        sync_batchnorm = True  #should be true for distributed training(as per momentum^2 paper
+    )
+
+    trainer.fit(model, train_loader)
+
+    #trainer.validate(model, dataloaders=val_loader)
+
+    #trainer.test(model, test_loader)
