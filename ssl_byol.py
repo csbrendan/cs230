c397a6df9852fd6d79c2d73f04ef5963c91ce242
csbrendan <ubuntu@ip-172-31-46-111.us-west-2.compute.internal>
2023-06-03 18:20:04 +0000

    forget to upload updated byol ssl pretraining

--- ssl_byol.py
+++ ssl_byol.py
@@ -5,44 +5,36 @@ from kornia import filters
 from kornia.geometry import transform as tf
 import torch
 from torch import nn, Tensor
-
 from typing import Union
-
 from copy import deepcopy
 from itertools import chain
 from typing import Dict, List
 import pytorch_lightning as pl
 from torch import optim
 import torch.nn.functional as f
-
 from os import cpu_count
-
 from torch.utils.data import DataLoader
 from torchvision.models import resnet18
-
-######MINE
 import medmnist
 from medmnist import INFO
-
 import os
 import argparse
 import multiprocessing
 from pathlib import Path
 from PIL import Image
 import numpy as np
-
 from torchvision import models, transforms
 from torch.utils.data import Dataset, DataLoader
 
 
-######### MY CONSTS
-BATCH_SIZE = 32   #I had 32 they may need 128
-IMAGE_SIZE = 28 
+# CONSTS
+BATCH_SIZE = 256   
+IMAGE_SIZE = 28
 IMAGE_EXTS = ['.jpg', '.png', '.jpeg']
 NUM_WORKERS = multiprocessing.cpu_count()
 print("NUM_WORKERS: ", NUM_WORKERS)
 
-############### DATASETS - C# load the data via my method from pneumoniamnist.py
+############### DATASETS - load the data via method from chestmnist.py
 from torchvision.transforms import ToTensor
 
 data_transform = transforms.Compose([
@@ -51,7 +43,7 @@ data_transform = transforms.Compose([
     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # Normalize for RGB
 ])
 
-data_flag = 'breastmnist'
+data_flag = 'chestmnist'
 info = INFO[data_flag]
 task = info['task']
 n_channels = info['n_channels']
@@ -63,14 +55,11 @@ TEST_DATASET = DataClass(split='test', transform=data_transform, download=False)
 
 # encapsulate data into dataloader form
 train_loader = DataLoader(dataset=TRAIN_DATASET, batch_size=BATCH_SIZE, shuffle=True)
-val_loader = DataLoader(dataset=TRAIN_DATASET, batch_size=2*BATCH_SIZE, shuffle=False)
+val_loader = DataLoader(dataset=TEST_DATASET, batch_size=2*BATCH_SIZE, shuffle=False) 
 test_loader = DataLoader(dataset=TEST_DATASET, batch_size=2*BATCH_SIZE, shuffle=False)
 
 
-
-
 ############Augment
-
 class RandomApply(nn.Module):
     def __init__(self, fn: Callable, p: float):
         super().__init__()
@@ -95,13 +84,7 @@ def default_augmentation(image_size: Tuple[int, int] = (28, 28)) -> nn.Module:
         ),
     )
 
-
-
-
-##########################Encoder WRapper
-
-
-
+############Encoder Wrapper
 def mlp(dim: int, projection_size: int = 256, hidden_size: int = 4096) -> nn.Module:
     return nn.Sequential(
         nn.Linear(dim, hidden_size),
@@ -157,10 +140,7 @@ class EncoderWrapper(nn.Module):
         return self._encoded    
 
 
-
-
-######################## BYOL n' TRAIN
-
+######################## BYOL & TRAIN
 def normalized_mse(x: Tensor, y: Tensor) -> Tensor:
     x = f.normalize(x, dim=-1)
     y = f.normalize(y, dim=-1)
@@ -186,7 +166,6 @@ class BYOL(pl.LightningModule):
             model, projection_size, hidden_size, layer=hidden_layer
         )
         self.predictor = nn.Linear(projection_size, projection_size, hidden_size)
-        #self.hparams = hparams   #error is AttributeError: can't set attribute from call: byol = BYOL(model, image_size=(28, 28))  
         self._target = None
 
         self.encoder(torch.zeros(2, 3, *image_size))
@@ -203,8 +182,6 @@ class BYOL(pl.LightningModule):
     def update_target(self):
         for p, pt in zip(self.encoder.parameters(), self.target.parameters()):
             pt.data = self.beta * pt.data + (1 - self.beta) * p.data
-
-    # --- Methods required for PyTorch Lightning only! ---
 
     def configure_optimizers(self):
         optimizer = getattr(optim, self.hparams.get("optimizer", "Adam"))
@@ -237,44 +214,24 @@ class BYOL(pl.LightningModule):
 
         return {"loss": loss}
 
-    ''' deprecated!!!!
-    @torch.no_grad()
-    def validation_epoch_end(self, outputs: List[Dict]) -> Dict:
-        val_loss = sum(x["loss"] for x in outputs) / len(outputs)
-        self.log("val_loss", val_loss.item())
-    '''
-
-    '''
-    @torch.no_grad()
-    def on_validation_epoch_end(self, *arg, **kwargs) -> Dict:
-        #val_loss = sum(x["loss"] for x in outputs) / len(outputs)
-        all_preds = torch.stack(self.validation_step_outputs)
-        #self.log("val_loss", val_loss.item())
-    '''
-
-
-
-
 
 
 
 model = resnet18(weights=True)
-model.eval()
+
 byol = BYOL(model, image_size=(28, 28))
 
 trainer = pl.Trainer(
-    max_epochs=50, 
-    #gpus=-1,
-    accumulate_grad_batches=2048 //32,  # // 128
-    #weights_summary=None,
+    max_epochs=1000,
+    accumulate_grad_batches=2048 //256,  # // 128
 )
 train_loader = DataLoader(
     TRAIN_DATASET,
-    batch_size=32, #128
+    batch_size=256, #128
     shuffle=True,
     drop_last=True,
 )
-trainer.fit(byol, train_loader, val_loader)        
 
- 
-torch.save(model.state_dict(), 'byol2_pretrained_breastmnist_50_epochs.pth')  
+trainer.fit(byol, train_loader, val_loader)
+
+torch.save(model.state_dict(), 'byol2_pretrained_chestmnist_batchsize_256_1000_epochs.pth')
